2dfcc35171a8b131049ff3984a90d69c18c34208
estebanLoza <estebanm.hloza@gmail.com>
2024-09-15 00:09:08 -0600

    Se comienza a subier los cambios del proyecto

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import NavBar from "./lib/NavBar.svelte";
+  
+
+
+  //Importación de imagenes Gastos
+  import PieGrafica from './lib/img/PieGrafica.png';
+</script>
+
+
+<NavBar/>
+
+<main>
+    <div id="Gastos">
+      <img src={PieGrafica} alt="Grafica pie para mostrar sobre la educacion financiera">
+      <div id="CuentaDinero">
+        <p>$1,543.52</p>
+        <p>Total de Gastos</p>
+        <p>1 Mayo --> Hoy</p>
+
+      </div>
+    </div>
+</main>
+
+<style>
+  main{
+    
+    width: 100%;
+    
+  }
+
+
+
+  
+</style>
---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,43 @@
+<script>
+  import logoBanorte from './img/logoBanorte.png';
+  import iconoNotificacion from './img/iconoNotificacion.png';
+</script>
+  
+
+<header>
+  <nav>
+    <img id="logoBanorte" src={logoBanorte} alt="Logo de Banco Banorte">
+    <img id="Notificacion" src={iconoNotificacion} alt="Icono de notificación">
+  </nav>
+</header>
+
+
+<style>
+  header{
+    background-color:#EB0029;
+    width: 100vw;
+    height:3.7rem;
+  }
+
+  nav{
+    /* border:1px solid green; */
+    width: auto;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding:0 6% 0 6%;
+  }
+
+
+  nav #logoBanorte{
+    width: 12%;
+  }
+
+  nav #Notificacion{
+    width: 6.5%;
+  }
+
+
+
+</style>
